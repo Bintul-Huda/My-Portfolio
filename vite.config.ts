b5ba03098d9d2beeb08f67cf92d0ab3a1249c2a4
Bintul <admin@Bintuls-MacBook-Pro.local>
2024-11-01 21:21:32 +0000

    Update message

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/My-Portfolio/", // This should match your GitHub repo name
   server: {
-    port: 3000, // You can specify the port if you prefer, default is 3000
-  },
+    port: 3000
+  }
 });
